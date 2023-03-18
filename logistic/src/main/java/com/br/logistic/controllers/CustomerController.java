a930c6cf0e8872632a0a412731387c369e0aa437
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-18 15:47:37 -0300

    find customer by id

--- logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
+++ logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
@@ -1,15 +1,17 @@
 package com.br.logistic.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.logistic.domain.model.Customer;
 import com.br.logistic.domain.repository.CustomerRepository;
 
 import lombok.AllArgsConstructor;
-
 
 @RestController
 @AllArgsConstructor
@@ -18,7 +20,17 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping("/customers")
-    public List<Customer> findAll(){
-      return customerRepository.findAll();
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public ResponseEntity<Customer> findOne(@PathVariable Long customerId) {
+        Optional<Customer> customer = customerRepository.findById(customerId);
+
+        if (customer.isPresent()) {
+            return ResponseEntity.ok(customer.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
