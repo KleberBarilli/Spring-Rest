dd7465e42b883bc2c68976c5ff1008692d4a4251
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-18 16:29:27 -0300

    add jakarta bean validation

--- logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
+++ logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.logistic.domain.model.Customer;
 import com.br.logistic.domain.repository.CustomerRepository;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -41,12 +41,12 @@ public class CustomerController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Customer create(@RequestBody Customer customer) {
+    public Customer create(@Valid @RequestBody Customer customer) {
         return customerRepository.save(customer);
     }
 
     @PutMapping("/{customerId}")
-    public ResponseEntity<Customer> update(@PathVariable Long customerId, @RequestBody Customer customer) {
+    public ResponseEntity<Customer> update(@Valid @PathVariable Long customerId, @RequestBody Customer customer) {
         if (!customerRepository.existsById(customerId)) {
             return ResponseEntity.notFound().build();
         }
--- logistic/src/main/java/com/br/logistic/domain/model/Customer.java
+++ logistic/src/main/java/com/br/logistic/domain/model/Customer.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,10 +25,16 @@ public class Customer {
     private Long id;
 
     @Column(name = "full_name")
+    @NotBlank
+    @Size(max = 255)
     private String name;
 
+    @NotBlank
+    @Email
     private String email;
-    
+
+    @NotBlank
+    @Size(max = 20)
     private String whatsapp;
 
 }
