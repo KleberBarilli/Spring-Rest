f23beec47ae2485070c3af4a5a07ceb1b3a60c36
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-18 15:51:54 -0300

    add req mapping

--- logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
+++ logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.logistic.domain.model.Customer;
@@ -13,17 +14,18 @@ import com.br.logistic.domain.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 
 @RestController
+@RequestMapping("/customers")
 @AllArgsConstructor
 public class CustomerController {
 
     private CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
+    @GetMapping()
     public List<Customer> findAll() {
         return customerRepository.findAll();
     }
 
-    @GetMapping("/customers/{customerId}")
+    @GetMapping("/{customerId}")
     public ResponseEntity<Customer> findOne(@PathVariable Long customerId) {
         return customerRepository.findById(customerId)
                 .map(customer -> ResponseEntity.ok(customer))
