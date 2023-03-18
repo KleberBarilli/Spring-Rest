4c20ff61392cb638bf7a0c19f9c016d0e28f8fc9
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-18 15:22:15 -0300

    jakarta persistence

--- logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
+++ logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
@@ -1,6 +1,5 @@
 package com.br.logistic.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.br.logistic.domain.model.Customer;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class CustomerController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/customers")
     public List<Customer> findAll(){
-        Customer customer = new Customer();
-
-        customer.setId(1L);
-        customer.setName("Kleber");
-        customer.setEmail("Kb@gmail.com");
-        customer.setWhatsapp("54499995999");
-        
-        return Arrays.asList(customer);
+      return manager.createQuery("from Customer", Customer.class).getResultList();
     }
 }
--- logistic/src/main/java/com/br/logistic/domain/model/Customer.java
+++ logistic/src/main/java/com/br/logistic/domain/model/Customer.java
@@ -1,14 +1,31 @@
 package com.br.logistic.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "full_name")
     private String name;
+
     private String email;
+    
     private String whatsapp;
 
 }
