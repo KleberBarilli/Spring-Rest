8eec261b53ab9e68ea7e0ee7aa64655746f42f98
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-18 16:09:38 -0300

    update customer

--- logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
+++ logistic/src/main/java/com/br/logistic/controllers/CustomerController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +42,15 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.CREATED)
     public Customer create(@RequestBody Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Customer> update(@PathVariable Long customerId, @RequestBody Customer customer) {
+        if (!customerRepository.existsById(customerId)) {
+            return ResponseEntity.notFound().build();
+        }
+        customer.setId(customerId);
+        customer = customerRepository.save(customer);
+        return ResponseEntity.noContent().build();
     }
 }
